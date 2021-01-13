a36b9339daa49b001a04e2aad3a5ac42ae128a24
Przemysław Biały <yhst@nykredit.dk>
2021-01-13 11:49:29 +0100

    prettier, example2 - basic validation

--- src/examples/example1.tsx
+++ src/examples/example1.tsx
@@ -1,38 +1,63 @@
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export const Example1 = () => {
+  const { register, handleSubmit } = useForm();
 
-  const {register, handleSubmit, errors} = useForm();
+  /*
+  Talk about:
+      - howto install
+      - how form's code looks like without react-hook-form
+      - what to add and where:
+        - useForm
+        - register to ref
+        - handleSubmit to onSubmit
+      - how code looks after adding react-hook-form
+      - show (in browser) that clicking on Submit triggerrs `triggerSubmit` function
+   */
 
   const triggerSubmit = (myData: any) => {
-      console.log(myData)
-  }
+    console.log(myData);
+    // make backend request here
+  };
 
   return (
     <div>
-        <h1>1) Howto setup basic form</h1>
-        <div>To install - run <span style={{backgroundColor: '#CCC', color: '#488'}}>npm install react-hook-form</span></div>
+      <h1>1) Howto setup basic form</h1>
+      <div>
+        To install - run{" "}
+        <span style={{ backgroundColor: "#CCC", color: "#488" }}>
+          npm install react-hook-form
+        </span>
+      </div>
 
-        <br/>
-        <br/>
+      <br />
+      <br />
 
-        <h3>We start with this:</h3>
+      <h3>We start with this:</h3>
 
-        <form>
-            <div>Name <input name='name'/></div>
-            <div>Surname <input name='surname'/></div>
-            <button type='submit'>Submit</button>
-        </form>
+      <form>
+        <div>
+          Name <input name="name" />
+        </div>
+        <div>
+          Surname <input name="surname" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
 
-        <br/>
-        <br/>
-        <h3>We add `ref` and `handleSubmit`, and we end up with this:</h3>
+      <br />
+      <br />
+      <h3>We add `ref` and `handleSubmit`, and we end up with this:</h3>
 
-        <form onSubmit={handleSubmit(triggerSubmit)}>
-            <div>Name <input name='name' ref={register}/></div>
-            <div>Surname <input name='surname' ref={register}/></div>
-            <button type='submit'>Submit</button>
-        </form>
+      <form onSubmit={handleSubmit(triggerSubmit)}>
+        <div>
+          Name <input name="name" ref={register} />
+        </div>
+        <div>
+          Surname <input name="surname" ref={register} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
--- src/examples/example2.tsx
+++ src/examples/example2.tsx
@@ -1,5 +1,73 @@
+import { useForm } from "react-hook-form";
+
 export const Example2 = () => {
-    return (
-        <div>Example 2 </div>
-    )
-}
+  const { register, handleSubmit, errors } = useForm();
+
+  /*
+    Talk about:
+        - howto add validation (inside `register`)
+        - howto show errors (on "required: true")
+            - take errors from `useForm()`
+            - errors.name on bottom
+        - different way to setup error message
+            - put it instead of `true`
+            - howto show error then
+        - If validator needs more than a bool (example - min) to set it - you need `value` and `message`
+        - NOTE that age validation won't trigger
+            - if empty
+            - if letters inside
+            - show that `triggerSubmit` will be called with empty age
+     */
+
+  const triggerSubmit = (myData: any) => {
+    // NOTE - this won't run if form has any errors
+    console.log(myData);
+  };
+
+  return (
+    <div>
+      <h1>2) Basic validation</h1>
+
+      <br />
+      <br />
+
+      <form onSubmit={handleSubmit(triggerSubmit)}>
+        <div>
+          Name
+          <input name="name" ref={register({ required: true })} />
+        </div>
+        <div>
+          Surname
+          <input
+            name="surname"
+            ref={register({
+              required: "Surname is required",
+            })}
+          />
+        </div>
+        <div>
+          Age
+          <input
+            name="age"
+            ref={register({
+              min: {
+                value: 18,
+                message: "You need to be an adult to enter this site",
+              },
+            })}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      <br />
+      <br />
+
+      <div>
+        {errors.name && <p>Name is required</p>}
+        {errors.surname && <p>{errors.surname.message}</p>}
+        {errors.age && <p>{errors.age.message}</p>}
+      </div>
+    </div>
+  );
+};
