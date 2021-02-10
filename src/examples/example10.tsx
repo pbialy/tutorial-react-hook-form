440141856d659dc4b5d528c39c4cc4d7e442c788
Przemysław Biały <yhst@nykredit.dk>
2021-02-10 14:15:59 +0100

    example 10 - setValue

---
+++ src/examples/example10.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export const example10title = "10) Set values";
+
+const data = [
+  { name: "John", age: "34" },
+  { name: "Bob", age: "19" },
+  { name: "Mike", age: "7" },
+];
+
+export const Example10 = () => {
+  /*
+      Talk about:
+        - We might want to base our inputs on some other data - we have something like that in ApiGui already
+        - To do that we use "setValue" two times (can't use it once)
+        - You can pass "{ shouldValidate: true }" in order to trigger validation after setting value
+      */
+
+  const { register, handleSubmit, formState, setValue } = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+  });
+
+  const triggerSubmit = (myData: any) => {
+    console.log(myData);
+  };
+
+  const handleDataImport = (personIndex: number) => {
+    setValue("name", data[personIndex].name);
+    setValue("age", data[personIndex].age);
+    // setValue("age", data[personIndex].age, { shouldValidate: true });
+  };
+
+  return (
+    <div>
+      <h1>{example10title}</h1>
+
+      <br />
+      <br />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>click</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{data[0].name}</td>
+            <td>{data[0].age}</td>
+            <td>
+              <button onClick={() => handleDataImport(0)}>import</button>
+            </td>
+          </tr>
+          <tr>
+            <td>{data[1].name}</td>
+            <td>{data[1].age}</td>
+            <td>
+              <button onClick={() => handleDataImport(1)}>import</button>
+            </td>
+          </tr>
+          <tr>
+            <td>{data[2].name}</td>
+            <td>{data[2].age}</td>
+            <td>
+              <button onClick={() => handleDataImport(2)}>import</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br />
+      <br />
+      <br />
+
+      <form onSubmit={handleSubmit(triggerSubmit)}>
+        <div>
+          Name
+          <input name="name" ref={register} />
+        </div>
+
+        <div>
+          Age
+          <input
+            name="age"
+            ref={register({
+              min: {
+                value: 18,
+                message: "You must be mature bro",
+              },
+            })}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <br />
+      <br />
+
+      {formState.errors.age && (
+        <p style={{ color: "red" }}>{formState.errors.age.message}</p>
+      )}
+      <br />
+    </div>
+  );
+};
