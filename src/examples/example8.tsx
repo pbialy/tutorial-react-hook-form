66dd1f5a2aacea4cfabe5a936eec31532658bce3
Przemysław Biały <yhst@nykredit.dk>
2021-02-10 12:14:30 +0100

    tries with debounce

--- src/examples/example8.tsx
+++ src/examples/example8.tsx
@@ -1,19 +1,24 @@
+import React from "react";
 import { useForm } from "react-hook-form";
+
+import debounce from "debounce";
 
 export const example8title = "8) Async validator";
 
-async function checkIfNameTaken(name: string) {
-  setTimeout(() => {
-    if (name === "Nykredit" || name === "Empik") {
-      return true;
-    } else {
-      return false;
-    }
-  }, 2000);
-}
-
 export const Example8 = () => {
-  const { register, handleSubmit, errors } = useForm({
+  /*
+      Talk about:
+        - We want to check if the name of the shop is taken WHILE inputting
+        - Looks like async validator only runs if normal validators are fine
+        - when field empty - async runs (add === '' check)
+        - problem - there's no debounce/throttle
+      */
+
+  // const { register, handleSubmit, errors } = useForm({
+  //   mode: "onChange",
+  //   reValidateMode: "onChange",
+  // });
+  const { register, handleSubmit, errors, trigger } = useForm({
     mode: "onChange",
     reValidateMode: "onChange",
   });
@@ -21,6 +26,8 @@ export const Example8 = () => {
   const triggerSubmit = (myData: any) => {
     console.log(myData);
   };
+
+  const [showLoader, setShowLoader] = React.useState(false);
 
   return (
     <div>
@@ -40,27 +47,58 @@ export const Example8 = () => {
                 message: "Shop's name must have at least 3 letters",
               },
               validate: {
-                asyncValidate: (value) => {
-                  if (value % 2 === 1) {
-                    return true;
+                isNameTaken: async (value) => {
+                  // if (value === "") {
+                  //   return true;
+                  // }
+
+                  setShowLoader(true);
+                  const isTaken = await checkIfNameTaken(value);
+                  setShowLoader(false);
+
+                  if (isTaken) {
+                    return `Sorry, name ${value} is already taken.`;
                   } else {
-                    return "Only numbers NOT divisible by 2";
+                    return true;
                   }
                 },
               },
             })}
+            onChange={debounce(async () => {
+              await trigger("shopName");
+            }, 200)}
           />
+          {showLoader && (
+            <div style={{ color: "#441" }}>Checking if name is taken...</div>
+          )}
         </div>
+        <br />
         <button type="submit">Submit</button>
       </form>
 
       <br />
       <br />
 
-      {errors.shopName && <p>{errors.shopName.message}</p>}
+      {errors.shopName && (
+        <p style={{ color: "red" }}>{errors.shopName.message}</p>
+      )}
       <br />
 
       {console.log(errors)}
     </div>
   );
 };
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function checkIfNameTaken(name: string) {
+  console.log("checkIfNameTaken called");
+  await sleep(2000);
+  if (name === "Nykredit" || name === "Empik") {
+    return true;
+  } else {
+    return false;
+  }
+}
